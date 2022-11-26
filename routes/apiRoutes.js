c2075b09a314cd9a3fc0382e2a9b147d55487785
Jacob Scobie <jscobie@focus-solutions.net>
2022-11-26 10:09:03 -0500

    loading changes complete of last 2 days

---
+++ routes/apiRoutes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+
+const fs = require('fs');
+
+// requesting the existing notes
+
+router.get('/notes', function(req, res) {
+    fs.readFile('../db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
+
+// posting note function route 
+
+router.post('/notes', function(req, res) {
+    const userNotes = req.body;
+
+    fs.readFile('../db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(userNotes);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('../db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Note Added');
+  });
+
+
+// delete note function route
+
+router.delete('/api/notes/:id', function(req, res) {
+    // Gets id number of note to delete
+    const deleteNote = req.params.id;
+    console.log(deleteNote);
+
+    fs.readFile('../db/db.json', (err, data) => {
+      if (err) throw err;
+
+      // Comparing each note's id to delete note
+      dbData = JSON.parse(data);
+      // for each function, comparing each note's id to the chosen_for_death variable
+      for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id === Number(deleteNote)) {
+          dbData.splice([i], 1);
+        }
+      }
+      console.log(dbData);
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('../db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    // Express response.status(204)
+    res.status(204).send();
+  });
+
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+// add the html static page routes here
+
+// Require Path funcitonality
+const path = require('path');
+
+// Set Router to be a router for express.js
+const router = require('express').Router();
+
+// GET Route for feedback page
+router.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+// GET Route for all other page requests to / (root)
+router.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
